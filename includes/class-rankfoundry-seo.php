dec4bd0dc8805011415d77e3cbc477177518d2ca
Kevin Fairbanks <kevin@kevinfairbanks.com>
2023-09-07 04:03:01 -0400

    initial commit

---
+++ includes/class-rankfoundry-seo.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * RankFoundry SEO Main Class
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
+class RankFoundry_SEO {
+
+    /**
+     * The unique identifier of this plugin.
+     */
+    protected $plugin_name;
+
+    /**
+     * The current version of the plugin.
+     */
+    protected $version;
+
+    /**
+     * Define the core functionality of the plugin.
+     */
+    public function __construct() {
+        if (defined('RANKFOUNDRY_SEO_VERSION')) {
+            $this->version = RANKFOUNDRY_SEO_VERSION;
+        } else {
+            $this->version = '1.0.0';
+        }
+        $this->plugin_name = 'rankfoundry-seo';
+
+        $this->load_dependencies();
+        $this->set_locale();
+        $this->initialize_update_checker();
+        $this->define_admin_hooks();
+        $this->define_public_hooks();
+    }
+
+    /**
+     * Load the required dependencies for this plugin.
+     */
+    private function load_dependencies() {
+        // Include other required files here
+        // For example, the API and sync classes, once they're set up.
+        require_once RANKFOUNDRY_SEO_PLUGIN_DIR . 'includes/class-rankfoundry-seo-api.php';
+        require_once RANKFOUNDRY_SEO_PLUGIN_DIR . 'includes/class-rankfoundry-seo-sync.php';
+    }
+
+    /**
+     * Define the locale for this plugin for internationalization.
+     */
+    private function set_locale() {
+        // This is where you'd set up any translation/internationalization functionality.
+    }
+
+    /**
+     * Load the update checker
+     */
+    private function initialize_update_checker() {
+        $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+            'https://github.com/rankfoundry/seo-plugin/',
+            RANKFOUNDRY_SEO_FILE,
+            'rankfoundry-seo'
+        );
+
+        //Set the branch that contains the stable release.
+        $myUpdateChecker->setBranch('master');
+
+        //Optional: If you're using a private repository, specify the access token like this:
+        //$myUpdateChecker->setAuthentication('your-token-here');
+    }
+
+    /**
+     * Register all of the hooks related to the admin area functionality.
+     */
+    private function define_admin_hooks() {
+        // This will involve enqueueing scripts/styles for the admin, and other related functions.
+    }
+
+    /**
+     * Register all of the hooks related to the public-facing functionality.
+     */
+    private function define_public_hooks() {
+        // This will involve enqueueing scripts/styles for the frontend, and other related functions.
+    }
+
+    /**
+     * Run the plugin.
+     */
+    public function run() {
+        // Any code that needs to execute upon plugin initialization would go here.
+    }
+}
+
---
+++ rankfoundry-seo.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * RankFoundry SEO Plugin
+ *
+ * @package   RankFoundry_SEO
+ * @link      https://rankfoundry.com
+ * @copyright Copyright (C) 2021-2023, Rank Foundry LLC - support@rankfoundry.com
+ * @since     1.0.0
+ * @license   GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: RankFoundry SEO
+ * Plugin URI:  https://rankfoundry.com/plugins/seo
+ * Description: An integration bridge between WordPress and RankFoundry SEO, enabling real-time data synchronization and updates via API.
+ * Version:     1.0.0
+ * Author:      Rank Foundry
+ * Author URI:  https://rankfoundry.com
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: rankfoundry-seo
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+// Define plugin version
+if (!defined('RANKFOUNDRY_SEO_VERSION')) {
+    define('RANKFOUNDRY_SEO_VERSION', '1.0.0');
+}
+
+// Define plugin directory path
+if (!defined('RANKFOUNDRY_SEO_PLUGIN_DIR')) {
+    define('RANKFOUNDRY_SEO_PLUGIN_DIR', plugin_dir_path(__FILE__));
+}
+
+// Define plugin file
+if ( ! defined( 'RANKFOUNDRY_SEO_FILE' ) ) {
+    define( 'RANKFOUNDRY_SEO_FILE', __FILE__ );
+}
+
+// Load the Composer autoloader.
+require_once RANKFOUNDRY_SEO_PLUGIN_DIR . 'vendor/autoload.php';
+
+// Include the main class file
+require_once RANKFOUNDRY_SEO_PLUGIN_DIR . 'includes/class-rankfoundry-seo.php';
+
+// Begin execution of the plugin.
+function run_rankfoundry_seo() {
+    $plugin = new RankFoundry_SEO();
+    $plugin->run();
+}
+
+run_rankfoundry_seo();
